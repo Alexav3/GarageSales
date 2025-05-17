ee943309ac4503c35a98edec713061055d2d20a3
Your Name <you@example.com>
2025-05-16 19:28:46 -0500

    git_ignore

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const db = require("./db");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const PORT = 3001;
 
-app.get("/", (req, res) => {
-  res.send("API is working!");
-});
+app.use(cors());
+app.use(express.json());
+app.use("/api", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
